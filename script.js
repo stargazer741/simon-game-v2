a47951051469ba717d7dd2a18add0649b7a23bba
stargazer7 <102627303+stargazer741@users.noreply.github.com>
2022-09-09 18:53:34 +0300

    game working
    main functions working, ready to final polishing

---
+++ script.js
@@ -0,0 +1,85 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+
+let level = 0;
+let started = false;
+
+$(".btnSimon").css("pointer-events", "none");
+
+$(document).keypress(function () {
+  if (!started) {
+    $(".title").text("Game Started!");
+    $("#score").text(level);
+    $(".btnSimon").css("pointer-events", "");
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btnSimon").click(function (event) {
+  let userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#score").text(level);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeOut(200)
+    .fadeIn(200);
+
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 200);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $(".title").text("Game Over, Press ANY Key to Restart");
+
+    $(".btnSimon").css("pointer-events", "none");
+
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
