bb3833ed7c01c978c362487709ef89aa7115810f
stargazer7 <102627303+stargazer741@users.noreply.github.com>
2022-09-10 04:52:32 +0300

    release

--- script.js
+++ script.js
@@ -82,3 +82,26 @@ function startOver() {
   gamePattern = [];
   started = false;
 }
+
+
+//modal and others
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("modalButton");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if(event.target == modal) {
+    modal.style.display = "none";
+  }
+}
